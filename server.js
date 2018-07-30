c4b00c626de8fa1cc5974f0458caae988d41d94a
Lucas Condon <lcondon@bennington.edu>
2018-07-30 19:01:44 -0400

    port heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 
 var app = express();
-var PORT = 8080;
+var PORT = process.env.PORT || 8080;
 
 app.get("/", function (req, res) {
     res.end("you have done it")
